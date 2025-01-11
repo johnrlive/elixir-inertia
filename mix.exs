ceb7363ca87a987411ad38461d34803102d709dc
Daniel Bergholz <bergholz.daniel@gmail.com>
2025-01-11 17:53:49 -0300

    Install `bcrypt_elixir`

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule Threadit.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:inertia, "~> 2.1.0"}
+      {:inertia, "~> 2.1.0"},
+      {:bcrypt_elixir, "~> 3.0"}
     ]
   end
 
